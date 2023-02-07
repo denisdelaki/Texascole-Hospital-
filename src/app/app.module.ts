6af757a725130a822208431d94e0402bad13d9dd
denisdelaki <denisdelaki@gmail.com>
2023-02-07 20:02:16 +0300

    created the doctor's page and style it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DoctorComponent } from './doctor/doctor.component';
@@ -43,6 +43,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
  
--- src/app/doctor/doctor.component.ts
+++ src/app/doctor/doctor.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./doctor.component.css']
 })
 export class DoctorComponent {
+  doctorName = 'Dr. John Doe';
+  specialization = 'Surgeon';
+  appointments = [    { date: '2022-03-01', time: '9:00 AM', patientName: 'Jane Doe' },    { date: '2022-03-01', time: '10:00 AM', patientName: 'John Smith' },    { date: '2022-03-02', time: '2:00 PM', patientName: 'Bob Johnson' },  ];
+  patients = [
+    { name: 'Jane Doe', dob: '01/01/1980', history: 'Treatment for flu' },
+    { name: 'John Smith', dob: '03/05/1985', history: 'Broken arm surgery' },
+    { name: 'Bob Johnson', dob: '06/22/1990', history: 'Regular check-ups' },
+    ];
+  updateDoctorInfo() {
+    // update doctor information in the database
+  }
 
+  rescheduleAppointment(appointment: any) {
+    // your code
+  }
+
+  viewPatientHistory(patient: any) {
+    // view patient history logic
+  }
 }
