b1ad19eaafa890a351a13070795a6fea031a2324
denisdelaki <denisdelaki@gmail.com>
2023-02-07 14:55:28 +0300

    initialized the project and created the Home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,49 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { ReceptionComponent } from './reception/reception.component';
+import { PatientComponent } from './patient/patient.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { HomeComponent } from './home/home.component';
 
+const routes: Routes = [
+  
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      { path: 'signin', component: SignInComponent },
+      { path: 'signup', component: SignUpComponent },
+    ]
+  },
+  {path: 'dashboard',
+  component: DashboardComponent,
+  children: [
+    { path: 'doctor', component: DoctorComponent },
+    { path: 'patient', component: PatientComponent },
+    { path: 'reception', component: ReceptionComponent }, 
+  ]}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    DoctorComponent,
+    ReceptionComponent,
+    PatientComponent,
+    SignUpComponent,
+    SignInComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
